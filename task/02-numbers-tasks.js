b741fc29725cc7b6a40aa87a03b96d26ba4116aa
Maria Averchenko <miss.elsa.brown@gmail.com>
2021-04-20 21:54:44 +0300

    isPrime

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -209,8 +209,14 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(n) {
-    throw new Error('Not implemented');
+
+ function isPrime(n) {
+    for (let i = 2; i < n; i++) {
+      if(n % i === 0) { 
+    return false;
+      }  
+    }
+    return n > 1;
 }
 
 /**
